328e7c029d97dc4c1417373e379ec8fc4d630bb3
emerson2pe <sala302b@ceines.com.br>
2019-09-02 20:09:41 -0300

    AtualizadoDia02

---
+++ Atividade1Java/app/src/main/java/br/senac/es/atividade1java/MainActivity.java
@@ -0,0 +1,16 @@
+package br.senac.es.atividade1java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
