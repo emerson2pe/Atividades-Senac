ec0c1138e67c348c9d4504323e3c45e3f5598fe7
emerson2pe <sala302b@ceines.com.br>
2019-09-04 20:09:02 -0300

    Atividade3

---
+++ Atividade3/src/atividade3/Atividade3.java
@@ -0,0 +1,22 @@
+
+package atividade3;
+
+
+public class Atividade3 {
+   
+    public static void main(String[] args) {
+        
+        
+        String string1 = "o rato roeu a roupa ".replace("a","1").replace("e","5").replace("i","9").replace("o","15").replace("u","21");
+        String string2 = " do rei de roma".replace("a","1").replace("e","5").replace("i","9").replace("o","15").replace("u","21");
+        
+       
+        
+        System.out.println(string1 + string2);       
+    
+    }
+    
+    
+    
+    
+}
